fa37277b1a11f002788b2ef1e3a7dbe3903b49ef
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-10-11 21:36:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.23%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-    return pow(x,n);
-        
+        long long exp = n;  // store n in long long to handle overflow
+        double ans = 1.0;
+
+    // if power is negative, make it positive for calculation
+    if (exp < 0) {
+        exp = -exp;
+        x = 1 / x;  // reciprocal for negative power
+    }
+
+    // fast exponentiation (O(log n))
+    while (exp > 0) {
+        if (exp % 2 == 1) ans = ans * x;
+        x = x * x;
+        exp = exp / 2;
+    }
+    return ans;
         
     }
 };
