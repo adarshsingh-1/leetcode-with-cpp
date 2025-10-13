3dbf27826e4bbf1ee90f12845a5509e312c48e50
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-10-13 23:28:50 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.85%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        int i = s.length() - 1;
+        while (i >= 0) {
+            // removing the brackets
+            while (i >= 0 && s[i] == ' ') {
+                i--;
+            }
+            // if the index is negative
+            if (i < 0) {
+                break;
+            }
+            // found the word end
+            int j = i;
+            // finding the word start
+            while (i >= 0 && s[i] != ' ') {
+                i--;
+            }
+            string word = s.substr(i + 1, j-i);
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+            ans += word;
+        }
+        return ans;
+    }
+};
