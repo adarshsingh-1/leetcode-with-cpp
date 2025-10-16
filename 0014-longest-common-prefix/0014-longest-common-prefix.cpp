f75a1eea0898fbe99e4a44de48d295ab7dff70e3
Adarsh Singh <112319359+adarshsingh-1@users.noreply.github.com>
2025-10-16 23:55:21 +0530

    Time: 0 ms (100%), Space: 12 MB (32.66%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        //taking the first element as answer and then comparing it to next strings
         string prefix = strs[0];
         int n = strs.size();
 
@@ -8,7 +9,7 @@ public:
                 string current = strs[i];
                 int j = 0;
             
-
+            //checking if the pointer is less than both prefix and current string and increasing the counter to calculate the length of the common 
             while (j < prefix.length() && j < current.length() && prefix[j] == current[j]) {
                 j++;
             }
@@ -16,7 +17,6 @@ public:
             // trim prefix to matched part
             prefix = prefix.substr(0, j);
 
-            // stop early if prefix becomes empty
             if (prefix.empty()) return "";
         }
 
